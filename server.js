f862e8f84197993f79a5ec92263e43dce532c215
RohitArora1807 <rohitarora0069@gmail.com>
2023-08-10 12:49:09 +0530

    make it better

--- server.js
+++ server.js
@@ -307,6 +307,6 @@ app.delete("/tickets/:TicketID", (req,res)=>{
  
 
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
